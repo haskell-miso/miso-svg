2e3acc1c71d66ab957eb23669910bce7fe194dde
dmjio <code@dmj.io>
2025-08-08 11:38:30 -0500

    Bump flake, use Miso.Lens, use new CSS combinators.

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import qualified Data.Map as M
 -----------------------------------------------------------------------------
 import           Miso hiding (update, text_)
 import           Miso.String (ms)
+import           Miso.Lens (this, (.=))
 import           Miso.Svg hiding (height_, id_, style_, width_)
 import qualified Miso.Style as CSS
 -----------------------------------------------------------------------------
@@ -18,8 +19,7 @@ foreign export javascript "hs_start" main :: IO ()
 -----------------------------------------------------------------------------
 main :: IO ()
 main = run $ startApp app
-  { events = M.insert "pointermove" False pointerEvents
-  , subs = [ mouseSub HandlePointer ]
+  { events = M.singleton "pointermove" False
   }
 -----------------------------------------------------------------------------
 -- | Component definition (uses 'component' smart constructor)
@@ -27,53 +27,48 @@ app :: App Model Action
 app = component emptyModel updateModel viewModel
 -----------------------------------------------------------------------------
 emptyModel :: Model
-emptyModel = Model (0, 0)
+emptyModel = (0, 0)
 -----------------------------------------------------------------------------
 updateModel :: Action -> Transition Model Action
-updateModel (HandlePointer pointer) = modify update
-  where
-    update m = m { mouseCoords = client pointer }
+updateModel (HandlePointer pointer) = this .= client pointer
 -----------------------------------------------------------------------------
-data Action
-  = HandlePointer PointerEvent
+data Action = HandlePointer PointerEvent
 -----------------------------------------------------------------------------
-newtype Model
-  = Model
-  { mouseCoords :: (Double, Double)
-  } deriving (Show, Eq)
+type Model = (Double, Double)
 -----------------------------------------------------------------------------
 viewModel :: Model -> View Model Action
-viewModel (Model (x, y)) =
-    div_
-        []
-        [ svg_
-            [ CSS.style_
-              [ CSS.borderStyle "solid"
-              , CSS.height "700px"
-              , CSS.width "100%"
-              ]
-            , onPointerMove HandlePointer
-            ]
-            [ g_
-                []
-                [ ellipse_
-                    [ cx_ $ ms x
-                    , cy_ $ ms y
-                    , CSS.style_
-                        [ "fill" =: "yellow"
-                        , "stroke" =: "purple"
-                        , "stroke-width" =: "2"
-                        ]
-                    , rx_ "100"
-                    , ry_ "100"
-                    ]
-                    []
-                ]
-            , text_
-                [ x_ $ ms x
-                , y_ $ ms y
-                ]
-                [text $ ms $ show (round x :: Int, round y :: Int)]
-            ]
+viewModel (x, y) =
+  div_
+    []
+    [ svg_
+      [ CSS.style_
+        [ CSS.borderStyle "solid"
+        , CSS.height "700px"
+        , CSS.width "100%"
         ]
+      , onPointerMove HandlePointer
+      ]
+      [ g_
+        []
+        [ ellipse_
+          [ cx_ (ms x)
+          , cy_ (ms y)
+          , CSS.style_
+            [ CSS.fill "yellow"
+            , CSS.stroke "purple"
+            , CSS.strokeWidth "2"
+            ]
+          , rx_ "100"
+          , ry_ "100"
+          ]
+          []
+        ]
+      , text_
+        [ x_ (ms x)
+        , y_ (ms y)
+        ]
+        [ text $ ms $ show (round x, round y)
+        ]
+      ]
+    ]
 -----------------------------------------------------------------------------
